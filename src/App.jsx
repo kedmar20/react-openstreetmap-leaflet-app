9179c456f0a5bb59b8a49104d741418069ace8b6
Kedmar20 <m.kedzierski76@gmail.com>
2023-08-28 20:58:33 +0200

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon, divIcon, point } from "leaflet";
+import iconPlace2 from "./assets/place2.svg";
+import iconPlace1 from "./assets/place.svg";
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 function App() {
-  const [count, setCount] = useState(0)
+   //markers:
+   const markers = [
+      {
+         geocode: [52.255422254652494, 10.539756513488964],
+         popUp: "BraWo Park",
+      },
+      {
+         geocode: [52.263672803998446, 10.518041051244222],
+         popUp: "Typografix",
+      },
+      {
+         geocode: [52.2902406843294, 10.521280911411937],
+         popUp: "Eintracht-Stadion",
+      },
+   ];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   const customIcon = new Icon({
+      iconUrl: iconPlace1,
+      iconSize: [68, 68], //size of the icon
+   });
+
+   // custom cluster icon
+   const createClusterCustomIcon = function (cluster) {
+      return new divIcon({
+         html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+         className: "custom-marker-cluster",
+         iconSize: point(63, 63, true),
+      });
+   };
+
+   return (
+      <MapContainer center={[52.266666, 10.516667]} zoom={13}>
+         <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+         />
+
+         <MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon}>
+            {markers.map((marker, i) => {
+               return (
+                  <Marker position={marker.geocode} icon={customIcon} key={i}>
+                     <Popup>{marker.popUp}</Popup>
+                  </Marker>
+               );
+            })}
+         </MarkerClusterGroup>
+      </MapContainer>
+   );
 }
 
-export default App
+export default App;
